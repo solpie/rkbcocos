b4a8ef225d359c1de606ab9bf27203cc1892a52a
solpie <solpie.net@gmail.com>
2019-04-16 09:52:13 +0800

    bg opacity

--- assets/Script/__clip.ts
+++ assets/Script/__clip.ts
@@ -5,7 +5,6 @@ export default class __Clip extends cc.Component {
 
     start() {
         var anim = this.getComponent(cc.Animation);
-
         // 如果没有指定播放哪个动画，并且有设置 defaultClip 的话，则会播放 defaultClip 动画
         anim.play("player_dot");
         cc.log('on start')
