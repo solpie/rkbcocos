92c5e635b3ff3fbc72914b7d3d99a60cfe3d1686
solpie <solpie.net@gmail.com>
2019-09-07 12:32:56 +0800

    blood bar

--- assets/Script/panel/big_blood_rock/BBR_player_bar.ts
+++ assets/Script/panel/big_blood_rock/BBR_player_bar.ts
@@ -3,6 +3,7 @@ import { InjectCls } from '../../__c';
 const { ccclass, property } = cc._decorator;
 declare let _c_;
 export let EVENT_PLAYER_BAR_4V4 = 'EVENT_PLAYER_BAR_4V4'
+const BAR_WIDTH = 395//359
 @ccclass
 export default class PlayerBar4v4 extends InjectCls {
     @property
@@ -24,12 +25,20 @@ export default class PlayerBar4v4 extends InjectCls {
             if (p) {
                 if (p.blood < 0)
                     p.blood = 0
-                this.bloodBar.x = this.bloodBarInitX + (p.blood / p.init_blood) * 359
+                this.bloodBar.x = this.bloodBarInitX - (p.blood / p.init_blood) * BAR_WIDTH
                 this.setNodeLabel('txt_player_name', p.name)
                 this.setSPbyUrl('avt', p.avatar)
                 this.setNodeLabel('txt_player_blood', p.blood || 0)
             }
         })
+        if (!CC_BUILD) {
+            this.test()
+        }
+    }
+
+    test() {
+        _c_.emit(EVENT_PLAYER_BAR_4V4, { 'R1': { blood: 3, init_blood: 6 ,name:'1',avatar:""} })
+        _c_.emit(EVENT_PLAYER_BAR_4V4, { 'L1': { blood: 3, init_blood: 6 ,name:'1',avatar:""} })
     }
 
 }
