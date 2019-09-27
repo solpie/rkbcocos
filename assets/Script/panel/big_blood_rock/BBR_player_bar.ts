4427d2d82d1473d3d04fd3aa5f97684dd50c37c7
solpie <solpie.net@gmail.com>
2019-09-27 13:46:53 +0800

    Update BBR_player_bar.ts
    bblood fallback

--- assets/Script/panel/big_blood_rock/BBR_player_bar.ts
+++ assets/Script/panel/big_blood_rock/BBR_player_bar.ts
@@ -3,7 +3,8 @@ import { InjectCls } from '../../__c';
 const { ccclass, property } = cc._decorator;
 declare let _c_;
 export let EVENT_PLAYER_BAR_4V4 = 'EVENT_PLAYER_BAR_4V4'
-const BAR_WIDTH = 395//359
+// const BAR_WIDTH = 395//benxi
+const BAR_WIDTH = 359
 @ccclass
 export default class PlayerBar4v4 extends InjectCls {
     @property
@@ -17,15 +18,16 @@ export default class PlayerBar4v4 extends InjectCls {
         let playerBar = cc.find("bar_mask/black_mask", this.node);
         console.log('4v4_player_bar', playerBar)
         this.bloodBar = playerBar
-        this.bloodBarInitX = this.bloodBar.x
-        // this.bloodBar.x = this.bloodBarInitX - (1 - 0 / this.initBlood) * 359
+        // this.bloodBarInitX = this.bloodBar.x//benxi
+        this.bloodBar.x = this.bloodBarInitX - (1 - 0 / this.initBlood) * 359
         _c_.on(EVENT_PLAYER_BAR_4V4, data => {
             // cc.log('playerBar', data)
             let p = data[this.idx]
             if (p) {
                 if (p.blood < 0)
                     p.blood = 0
-                this.bloodBar.x = this.bloodBarInitX - (p.blood / p.init_blood) * BAR_WIDTH
+                // this.bloodBar.x = this.bloodBarInitX - (p.blood / p.init_blood) * BAR_WIDTH
+                this.bloodBar.x = this.bloodBarInitX + (p.blood / p.init_blood) * BAR_WIDTH
                 this.setNodeLabel('txt_player_name', p.name)
                 this.setSPbyUrl('avt', p.avatar)
                 this.setNodeLabel('txt_player_blood', p.blood || 0)
