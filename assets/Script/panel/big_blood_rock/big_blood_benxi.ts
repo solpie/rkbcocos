b0bb7419e2a24ea1b04a384f5cf9536bcb4859f1
solpie <solpie.net@gmail.com>
2019-10-28 14:31:53 +0800

    blood

--- assets/Script/panel/big_blood_rock/big_blood_benxi.ts
+++ assets/Script/panel/big_blood_rock/big_blood_benxi.ts
@@ -15,14 +15,14 @@ export default class BigBloodRock extends cc.Component {
     rightTeamMap: any
     // timeout_dot_L = []
     // timeout_dot_R = []
-    timeout_L:cc.Label
-    timeout_R:cc.Label
-    total_blood_L:cc.Label
-    total_blood_R:cc.Label
-    foul_L:cc.Label
-    foul_R:cc.Label
-    captain_blood_L:cc.Label
-    captain_blood_R:cc.Label
+    timeout_L: cc.Label
+    timeout_R: cc.Label
+    total_blood_L: cc.Label
+    total_blood_R: cc.Label
+    foul_L: cc.Label
+    foul_R: cc.Label
+    captain_blood_L: cc.Label
+    captain_blood_R: cc.Label
 
     player_row_L = []
     player_row_R = []
@@ -31,14 +31,14 @@ export default class BigBloodRock extends cc.Component {
     }
     start() {
 
-        let _Label = (name)=>{
-            return cc.find(name,this.node).getComponent(cc.Label)
+        let _Label = (name) => {
+            return cc.find(name, this.node).getComponent(cc.Label)
         }
-        let _Sp = (name)=>{
-            return cc.find(name,this.node).getComponent(cc.Sprite)
+        let _Sp = (name) => {
+            return cc.find(name, this.node).getComponent(cc.Sprite)
         }
-        this.timeout_L = cc.find('txt_timeout_L',this.node).getComponent(cc.Label)
-        this.timeout_R = cc.find('txt_timeout_R',this.node).getComponent(cc.Label)
+        this.timeout_L = cc.find('txt_timeout_L', this.node).getComponent(cc.Label)
+        this.timeout_R = cc.find('txt_timeout_R', this.node).getComponent(cc.Label)
         this.timeout_L.string = '2'
         this.timeout_R.string = '2'
 
@@ -54,15 +54,14 @@ export default class BigBloodRock extends cc.Component {
         // let blood = _Label('player_bar_benxi_R1/txt_player_blood')
         // blood.string = '9'
         for (let i = 0; i < 4; i++) {
-            let idx = (i+1)
+            let idx = (i + 1)
             let blood_L = _Label(`player_bar_benxi_L${idx}/txt_player_blood`)
             let blood_R = _Label(`player_bar_benxi_R${idx}/txt_player_blood`)
             let avt_L = _Sp(`player_bar_benxi_L${idx}/avt`)
             let avt_R = _Sp(`player_bar_benxi_R${idx}/avt`)
-            this.player_row_L.push({blood:blood_L,avatar:avt_L})
-            this.player_row_R.push({blood:blood_R,avatar:avt_R})
-            if(blood_L)
-            {
+            this.player_row_L.push({ blood: blood_L, avatar: avt_L, player_id: '' })
+            this.player_row_R.push({ blood: blood_R, avatar: avt_R, player_id: '' })
+            if (blood_L) {
                 blood_L.string = '9'
                 blood_R.string = '9'
             }
@@ -103,17 +102,18 @@ export default class BigBloodRock extends cc.Component {
                 let leftTeamMap = {}
                 let is5v5 = leftTeam.length == 5
                 let row_idx_L = 0, row_idx_R = 0
-                let captain_player_id_L,captain_player_id_R
+                let captain_player_id_L, captain_player_id_R
                 for (let i = 0; i < leftTeam.length; i++) {
                     let p = leftTeam[i];
                     let pid = p.player_id
                     if (is5v5 && pid == leftPlayer.player_id) { continue; }
-                    
+
                     let player_row = this.player_row_L[i]
                     player_row.blood.string = p.blood
-                    loadImg64ByNode(player_row.avatar,p.avatar)
+                    player_row.player_id = pid
+                    loadImg64ByNode(player_row.avatar, p.avatar)
                 }
-                 let rightTeam = data.rTeam
+                let rightTeam = data.rTeam
                 for (let i = 0; i < rightTeam.length; i++) {
                     let p = rightTeam[i];
                     let pid = p.player_id
@@ -121,33 +121,17 @@ export default class BigBloodRock extends cc.Component {
 
                     let player_row = this.player_row_R[i]
                     player_row.blood.string = p.blood
-                    loadImg64ByNode(player_row.avatar,p.avatar)
+                    player_row.player_id = pid
+                    loadImg64ByNode(player_row.avatar, p.avatar)
                 }
-                // _c_.emit(EVENT_PLAYER_BAR_4V4, leftTeamMap)
-                // this.leftTeamMap = leftTeamMap
-                // let rightTeam = data.rTeam
-                // let rightTeamMap = {}
-                // for (let i = 0; i < rightTeam.length; i++) {
-                //     let p = rightTeam[i];
-                //     let pid = p.player_id
-                //     if (is5v5 && pid == rightPlayer.player_id) {
-                //         continue;
-                //     }
-                //     // p.avatar = baseUrl + pid + '.png'
-                //     rightTeamMap['R' + (row_idx_R + 1)] = p
-                //     row_idx_R++
-                // }
-                // this.rightTeamMap = rightTeamMap
-                // _c_.emit(EVENT_PLAYER_BAR_4V4, rightTeamMap)
             })
             .on(WSEvent.sc_timeOut, data => {
                 cc.log('sc_timeOut', data)
                 this.set_timeout(data)
             })
             .on(WSEvent.sc_setFoul, data => {
-                setText('txt_player_foul_L', data.lFoul)
-                setText('txt_player_foul_R', data.rFoul)
-
+                this.foul_L.string = data.lFoul
+                this.foul_R.string = data.rFoul
             })
             .on(WSEvent.sc_manual_blood, data => {
                 cc.log('sc_manual_blood', data)
@@ -156,19 +140,17 @@ export default class BigBloodRock extends cc.Component {
                 for (let p of data.lTeam) {
                     let bar_player = this.findPlayerOnBar(p.player_id)
                     if (bar_player)
-                        bar_player.blood = p.blood
-                    if (p.player_id == data.vsPlayerArr[0])
-                        setText('txt_player_blood_L', p.blood)
+                        bar_player.blood.string = p.blood
+                    // if (p.player_id == data.vsPlayerArr[0])
+                    //     setText('txt_player_blood_L', p.blood)
                 }
                 for (let p of data.rTeam) {
                     let bar_player = this.findPlayerOnBar(p.player_id)
                     if (bar_player)
-                        bar_player.blood = p.blood
-                    if (p.player_id == data.vsPlayerArr[1])
-                        setText('txt_player_blood_R', p.blood)
+                        bar_player.blood.string = p.blood
+                    // if (p.player_id == data.vsPlayerArr[1])
+                    //     setText('txt_player_blood_R', p.blood)
                 }
-                _c_.emit(EVENT_PLAYER_BAR_4V4, this.leftTeamMap)
-                _c_.emit(EVENT_PLAYER_BAR_4V4, this.rightTeamMap)
             })
     }
     set_timeout(data) {
@@ -176,19 +158,13 @@ export default class BigBloodRock extends cc.Component {
         this.timeout_R.string = data.timeout_R
     }
     findPlayerOnBar(pid) {
-        for (let barIdx in this.leftTeamMap)//pos L1 L2
-        {
-            let player = this.leftTeamMap[barIdx]
-            if (pid == player.playerId) {
-                return player
-            }
+        for (let row of this.player_row_L) {
+            if (pid == row.player_id)
+                return row
         }
-        for (let barIdx in this.rightTeamMap)//pos L1 L2
-        {
-            let player = this.rightTeamMap[barIdx]
-            if (pid == player.playerId) {
-                return player
-            }
+        for (let row of this.player_row_R) {
+            if (pid == row.player_id)
+                return row
         }
     }
 }
