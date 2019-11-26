9a5de0b3386c7cd331d499ccf517d6075df98b42
guoyang@hupu.com <solpie.net@gmail.com>
2019-11-26 12:44:54 +0800

    mm

--- assets/Script/panel/rank5/rank5.ts
+++ assets/Script/panel/rank5/rank5.ts
@@ -17,15 +17,17 @@ function bubbleSort(arr) {
     return arr;
 }
 function swap_anim(item_i, item_i_1, arr, i, j, swap_anim) {
-
+    let item_i_scale = arr[item_i].is_max?1.3:1;
+    let item_i_1_scale = arr[item_i_1].is_max?1.3:1;
     cc['tween'](arr[item_i].node)
         .to(0.2, { y: ITEM_start_Y - item_i * ITEM_INVERT_Y })
-        .to(0.2, { scale:1.3 })
+        .to(0.2, { scale:item_i_scale })
         .start()
     cc['tween'](arr[item_i_1].node)
         .to(0.2, { y: ITEM_start_Y - item_i_1 * ITEM_INVERT_Y })
-        .to(0.2, { scale:1 })
+        .to(0.2, { scale:item_i_1_scale })
         .call(_ => {
+            arr[0].node.scale = 1.3
             bubble_anim(arr, i, j, swap_anim)
         })
         .start()
@@ -64,7 +66,7 @@ export default class NewClass extends cc.Component {
         for (let i = 0; i < 4; i++) {
             let player_node: cc.Node = cc.find('p' + (i + 1), this.node)
             if (player_node) {
-                this.player_node_arr.push({ node: player_node, vote: 0, name: '球员 ' + (i + 1), player_id: '' })
+                this.player_node_arr.push({ node: player_node, vote: 0, name: '球员 ' + (i + 1), player_id: '',is_max:false })
                 player_node.y = ITEM_start_Y - i * ITEM_INVERT_Y
                 cc.log('node', player_node.name)
             }
@@ -73,6 +75,7 @@ export default class NewClass extends cc.Component {
         auto_doc(get_rank5_doc_url, res => {
             if (res && res.length == 1) {
                 let doc = res[0].data
+                let max_score = 0
                 for (let i = 0; i < this.player_node_arr.length; i++) {
                     const item = this.player_node_arr[i];
                     let player
@@ -97,11 +100,18 @@ export default class NewClass extends cc.Component {
                     item.vote = Number(player.score)
                     if (item.player_id == doc.last_player_id) {
                         item.vote += 0.1
+                        // item.node.scale = 1
                     }
+
+                    max_score = Math.max(max_score,item.vote)
                     let score = cc.find('score', item.node).getComponent(cc.Label)
                     score.string =Math.floor( item.vote)+''
-                    bubble_anim(this.player_node_arr, 0, 0, swap_anim)
                 }
+                for (let i = 0; i < this.player_node_arr.length; i++) {
+                    const item = this.player_node_arr[i];
+                    item.is_max = item.vote==max_score
+                }
+                bubble_anim(this.player_node_arr, 0, 0, swap_anim)
             }
         })
 
