0f96a393b3f949be7a29ecc7571741618013595d
guoyang@hupu.com <solpie.net@gmail.com>
2019-11-28 17:36:40 +0800

    rank0

--- rkb/static/cc/web-mobile/main.eb58b.js
+++ rkb/static/cc/web-mobile/main.eb58b.js
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.266ea.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.eb58b.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -200,12 +200,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.290c1.js');
+        require('src/settings.f6d4a.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.290c1.js');
+        require('src/settings.f6d4a.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
